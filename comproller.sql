77b3db7221b144fa047293d39b5e8bdd52b98fa1
Papp Gyula <144594449+guts7777@users.noreply.github.com>
2025-03-19 11:21:50 +0100

    Teszt adatok frissítése.

--- comproller.sql
+++ comproller.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Már 18. 11:47
+-- Létrehozás ideje: 2025. Már 19. 09:45
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -58,19 +58,6 @@ CREATE TABLE `csekkolasok` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- A tábla adatainak kiíratása `csekkolasok`
---
-
-INSERT INTO `csekkolasok` (`DolgozoID`, `Vezeteknev`, `Keresztnev`, `Datum_Be`, `Datum_Ki`, `Kezdido`, `Vegido`, `Ora`, `Ber`, `Bonusz`, `Vegosszeg`, `created_at`, `updated_at`) VALUES
-(1, 'Kovács', 'János', '2025-03-01', '2025-03-01', '10:00', '12:00', NULL, NULL, NULL, NULL, '2025-03-14 14:40:58', '2025-03-14 14:46:24'),
-(2, 'Nagy', 'Anna', '2025-03-02', '2025-03-02', '10:00', '12:00', NULL, NULL, NULL, NULL, '2025-03-14 14:43:21', '2025-03-14 14:46:32'),
-(3, 'Szabó', 'Péter', '2025-03-03 ', '2025-03-03 ', '10:00', '12:00', NULL, NULL, NULL, NULL, '2025-03-14 15:19:10', '2025-03-14 15:19:10'),
-(4, 'Kovács', 'János', '2025-03-02 ', '2025-03-02 ', '15:00', '15:50', NULL, NULL, NULL, NULL, '2025-03-14 15:57:23', '2025-03-14 15:57:23'),
-(5, 'Kovács', 'János', '2025-03-03 ', '2025-03-03 ', '10:00', '12:00', NULL, NULL, NULL, NULL, '2025-03-14 15:57:42', '2025-03-14 15:57:42'),
-(6, 'Nagy', 'Anna', '2025-03-01 ', '2025-03-01 ', '10:00', '12:00', NULL, NULL, NULL, NULL, '2025-03-14 20:26:48', '2025-03-14 20:26:48'),
-(7, 'Nagy', 'Anna', '2025-03-01 ', '2025-03-01 ', '10:00', '12:00', NULL, NULL, NULL, NULL, '2025-03-14 20:29:20', '2025-03-14 20:29:20');
-
 -- --------------------------------------------------------
 
 --
@@ -84,6 +71,14 @@ CREATE TABLE `esemenyek` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- A tábla adatainak kiíratása `esemenyek`
+--
+
+INSERT INTO `esemenyek` (`id`, `datum`, `esemeny_neve`, `created_at`, `updated_at`) VALUES
+(1, '2025-03-05', 'valami1', NULL, NULL),
+(2, '2025-04-10', 'valami2', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -107,10 +102,7 @@ CREATE TABLE `felhasznalok` (
 INSERT INTO `felhasznalok` (`id`, `felhasznalonev`, `jelszo`, `szerep`, `created_at`, `updated_at`) VALUES
 (1, 'nikecareer', '123456', 'hr', NULL, NULL),
 (2, 'shellpath', '654321', 'pu', NULL, NULL),
-(3, 'viola123', 'admin123', 'valami', NULL, NULL),
-(4, 'rudirudi', '12345678', 'raktar', '2025-03-18 09:32:07', '2025-03-18 09:32:07'),
-(5, 'guest', 'guest123', 'guest', '2025-03-18 09:40:06', '2025-03-18 09:40:06'),
-(6, 'valami', 'valamivalami', 'admin', '2025-03-18 09:43:08', '2025-03-18 09:43:08');
+(3, 'viola123', 'admin123', 'admin', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -124,19 +116,6 @@ CREATE TABLE `ideiglenes` (
   `Datum_Be` varchar(255) NOT NULL,
   `Datum_Ki` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- A tábla adatainak kiíratása `ideiglenes`
---
-
-INSERT INTO `ideiglenes` (`DolgozoID`, `Nev`, `Datum_Be`, `Datum_Ki`) VALUES
-(1, 'Kovács János', '2025-03-01', '2025-03-01'),
-(2, 'Nagy Anna', '2025-03-01 ', '2025-03-01 '),
-(3, 'Nagy Anna', '2025-03-01 ', '2025-03-01 '),
-(4, 'Nagy Anna', '2025-03-02', '2025-03-02'),
-(5, 'Kovács János', '2025-03-02 ', '2025-03-02 '),
-(6, 'Szabó Péter', '2025-03-03 ', '2025-03-03 '),
-(7, 'Kovács János', '2025-03-03 ', '2025-03-03 ');
 
 -- --------------------------------------------------------
 
@@ -191,11 +170,8 @@ CREATE TABLE `nyilvantartas` (
 --
 
 INSERT INTO `nyilvantartas` (`DolgozoID`, `Keresztnev`, `Vezeteknev`, `Szuletesi_datum`, `Anyja_neve`, `Tajszam`, `Adoszam`, `Bankszamlaszam`, `Alapber`, `Cim`, `Allampolgarsag`, `Tartozkodasi_hely`, `Szemelyigazolvany_szam`, `Email`, `Telefonszam`, `Munkakor`, `Megjegyzes`, `Qrcode`, `created_at`, `updated_at`) VALUES
-(1, 'János', 'Kovács', '1990-01-01', 'Erzsébet Kovács', '123456789', '123456789', 'HU123456789', '250000', 'Budapest, Fő utca 1.', 'Magyar', 'Budapest', 'AB123456', 'janos.kovacs@example.com', '0612345678', 'Számviteli munkatárs', 'Nincs megjegyzés', 'QR1234567890', '2025-03-05 17:13:57', '2025-03-05 17:13:57'),
-(2, 'Anna', 'Nagy', '1985-05-12', 'Mária Nagy', '987654321', '987654321', 'HU987654321', '280000', 'Budapest, Petőfi utca 2.', 'Magyar', 'Budapest', 'XY987654', 'anna.nagy@example.com', '0612345679', 'HR munkatárs', 'Nincs megjegyzés', 'QR9876543210', '2025-03-05 17:13:57', '2025-03-05 17:13:57'),
-(3, 'Péter', 'Szabó', '1992-03-03', 'Katalin Szabó', '111223344', '112233445', 'HU112233445', '320000', 'Debrecen, Kossuth utca 5.', 'Magyar', 'Debrecen', 'XY112233', 'peter.szabo@example.com', '0623456789', 'Fejlesztő', 'Nincs megjegyzés', 'QR1122334455', '2025-03-05 17:13:57', '2025-03-14 14:41:45'),
-(4, 'Dolgozó', 'Példa', '2025-03-10', 'Példa Ember', '123456788', '12345678-1-15', '10000000-00000000-000000', '3000', '1000, Példa utca 01.', 'példa', '0000, Példa Példa utca 00.', 'dolgozo@gmail.com', 'dolgozo@gmail.com', '1234567891', 'Búrkoló', 'asd', 'xzw9dmmm', '2025-03-13 15:20:18', '2025-03-14 14:10:42'),
-(5, 'Új', 'Dolgozó', '2025-03-14', 'Példa Anyja', '111111111', '12345678-1-30', '123456789012', '3000', 'Példa út 30.', 'magyar', 'Példa.', '111111ZZ', 'pelda2@gmail.com', '06301111111', 'Búrkoló', 'Példa', '02t2zgm0', '2025-03-14 14:12:30', '2025-03-14 14:12:30');
+(1, 'Márk', 'Tóth', '1998-06-01', 'Tóth Mariska', '123456777', '12345678-1-15', '123456789012', '3000', 'Petőfi út 1.', 'magyar', 'Példa.', 'toth@gmail', 'toth@gmail', '06301111111', 'Szoftver Mérnök', 'Példa.', 'mr25spj9', '2025-03-19 07:38:32', '2025-03-19 07:41:54'),
+(2, 'Máté', 'Papp', '2000-08-10', 'Papp Anna', '123456789', '12345678-1-25', '123456789030', '5000', 'Petőfi út 2.', 'orosz', '11 Példa, Petőfi út 2.', '123456FF', 'mate@gmail', '+36301111111', 'HR Fejlesztő', 'Megjegyzés.', 'did3d194', '2025-03-19 07:44:54', '2025-03-19 07:44:54');
 
 -- --------------------------------------------------------
 
@@ -217,7 +193,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('Czm6EDt4Gy8Jg65wZL1DV4I08AYT1YcrVJdUO8zS', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjY6Il90b2tlbiI7czo0MDoiSlI4TVkzMmptSDRTTHhIY2VyNVdsS2hwM0lHT0U4OXNHTjhIT2NkQiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjk6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9wcm9maWxlIjt9czo3OiJpc0FkbWluIjtiOjE7fQ==', 1742294763);
+('gE1xp5HZ0LcmPDfaW4V5KjdHQvfKirYA4N0CpRul', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjY6Il90b2tlbiI7czo0MDoiR0NGeDRhSXRhQklrOWdxYkozN3RGdmY4VkI4VnppUDJMQWNlSHE0UCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzA6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9yZWdpc3RyeSI7fXM6NToiYWRtaW4iO2I6MTt9', 1742373895);
 
 --
 -- Indexek a kiírt táblákhoz
@@ -284,25 +260,25 @@ ALTER TABLE `sessions`
 -- AUTO_INCREMENT a táblához `csekkolasok`
 --
 ALTER TABLE `csekkolasok`
-  MODIFY `DolgozoID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `DolgozoID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT a táblához `esemenyek`
 --
 ALTER TABLE `esemenyek`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT a táblához `felhasznalok`
 --
 ALTER TABLE `felhasznalok`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT a táblához `ideiglenes`
 --
 ALTER TABLE `ideiglenes`
-  MODIFY `DolgozoID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `DolgozoID` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT a táblához `migrations`
@@ -314,7 +290,7 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT a táblához `nyilvantartas`
 --
 ALTER TABLE `nyilvantartas`
-  MODIFY `DolgozoID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `DolgozoID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
