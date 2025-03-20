d1406134cf28e2f25ce80f6cca0859e3188663bc
Papp Gyula <144594449+guts7777@users.noreply.github.com>
2025-03-20 11:43:16 +0100

    Komment hozzáadása.

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -7,7 +7,7 @@ use App\Models\Dolgozo;
 
 class WorkController extends Controller
 {
-    public function getJobTitles()
+    public function getJobTitles() // Munkakör címeinek kikérése.
     {
         $jobTitles = Dolgozo::pluck('munkakor');
         return response()->json($jobTitles);
