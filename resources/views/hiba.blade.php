483e8fe09a408e1f38df0dfb4e5fe027b11ec5b6
Papp Gyula <144594449+guts7777@users.noreply.github.com>
2025-03-20 11:41:38 +0100

    Favicon hozzáadása.

--- resources/views/hiba.blade.php
+++ resources/views/hiba.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hiba!</title>
     <link rel="stylesheet" href="{{ asset('css/hiba.css') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('kepek/icon.png') }}">
 </head>
 <div class="container">
     <h1>Hiba történt!</h1>
