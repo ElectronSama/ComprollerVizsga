d93cf1edd47a262f825c1c609cdf9edce967295c
Papp Gyula <144594449+guts7777@users.noreply.github.com>
2025-03-26 10:48:51 +0100

    Adatbázis nevek frissítése.

--- resources/views/munkaido.blade.php
+++ resources/views/munkaido.blade.php
@@ -256,7 +256,7 @@
                                             <tbody>
                                                 <?php
                                         
-                                                    $sql = "SELECT DolgozoID, Nev, Datum_Be, Datum_Ki FROM ideiglenes";
+                                                    $sql = "SELECT CsekkolasID, Nev, Datum_Be, Datum_Ki FROM ideiglenes";
                                                     $eredmeny = $kapcsolodas->query($sql);
                                             
                                                     if ($eredmeny->num_rows > 0) 
@@ -264,7 +264,7 @@
                                                         while ($sor = $eredmeny->fetch_assoc()) 
                                                         {
                                                             echo '<tr>';
-                                                            echo '    <td>' . $sor["DolgozoID"] . '</td>';
+                                                            echo '    <td>' . $sor["CsekkolasID"] . '</td>';
                                                             echo '    <td>' . $sor["Nev"] . '</td>';
                                                             echo '    <td>' . $sor["Datum_Be"] . '</td>';
                                                             echo '    <td>' . $sor["Datum_Ki"] . '</td>';
