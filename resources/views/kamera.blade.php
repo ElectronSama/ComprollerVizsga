09c896685984a42f3538c8dcd676196ef7927b57
Kiss Bercel <kiss.berci20@gmail.com>
2025-03-26 23:02:12 +0100

    kamera css

--- resources/views/kamera.blade.php
+++ resources/views/kamera.blade.php
@@ -4,12 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Comproller - QR Kód Olvasó</title>
+    <link rel="stylesheet" href="{{ asset('css/kameraV2.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body class="d-flex align-items-center justify-content-center vh-100">
     <div class="container">
         <div class="row justify-content-center">
+            <div class="text-center mb-4">
+                <a href="/" class="btn btn-light rounded-pill px-4 py-2">
+                    <i class="bi bi-moon-stars-fill me-2"></i>
+                    Vissza
+                </a>
+            </div>
             <div class="col-md-6">
                 <div class="card shadow-lg border-0">
                     <div class="card-body text-center">
