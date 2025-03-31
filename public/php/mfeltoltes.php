36a353a8dcbe01e758ed5e5374df6436bef0dae2
Papp Gyula <144594449+guts7777@users.noreply.github.com>
2025-03-31 17:38:55 +0200

    Új mezők hozzáadása.

--- public/php/mfeltoltes.php
+++ public/php/mfeltoltes.php
@@ -21,19 +21,16 @@
     $keresztnev = $_POST['keresztnev'];
     $az_id;
 
-    $sql = "SELECT Alapber, DolgozoID FROM nyilvantartas WHERE Vezeteknev = '$vezeteknev' AND Keresztnev = '$keresztnev'";
+    $sql = "SELECT DolgozoID FROM nyilvantartas WHERE Vezeteknev = '$vezeteknev' AND Keresztnev = '$keresztnev'";
     $eredmeny = $kapcsolat->query($sql);
 
     if ($sor = $eredmeny->fetch_assoc()) 
     {
-        $valasz["alapber"] = $sor["Alapber"];
         $az_id = $sor["DolgozoID"];
     } 
     else 
     {
-        $valasz["alapber_error"] = "Nincs ilyen dolgozó";
         $kapcsolat->close();
-        echo json_encode($valasz);
         exit();
     }
 
@@ -43,25 +40,29 @@
     $idopont_ido2 = $_POST['idopont_ido2'];
     $leiras_input = $_POST['leiras_input'];
     $muszak_input = $_POST['muszak_input'];
+    $ora_input = $_POST['ora_input'];
+    $ber_input = $_POST['ber_input'];
+    $bonusz_input = $_POST['bonusz_input'];
+    $vegosszeg_input = $_POST['vegosszeg_input'];
 
     if ($idopont_ido === "") 
     {
         $idopont_ido = "00:00";
     }
 
-    $idopont = $idopont_input . " ";
-    $idopont2 = $idopont_input2 . " ";
+    $idopont = $idopont_input;
+    $idopont2 = $idopont_input2;
 
-    $ora1 = $idopont_ido;
-    $ora2 = $idopont_ido2;
+    $ora1 = $idopont_ido . ":00";
+    $ora2 = $idopont_ido2 . ":00";
 
     $nevek = explode(" ", $muszak_input, 2);
 
     $vezeteknev = $nevek[0];
     $keresztnev = $nevek[1];
 
-    $sql = "INSERT INTO csekkolasok (az_id, Vezeteknev, Keresztnev, Datum_Be, Datum_Ki, Kezdido, Vegido) 
-            VALUES ('$az_id','$vezeteknev', '$keresztnev', '$idopont', '$idopont2', '$ora1', '$ora2')";
+    $sql = "INSERT INTO csekkolasok (az_id, Vezeteknev, Keresztnev, Datum_Be, Datum_Ki, Kezdido, Vegido, Ora, Ber, Bonusz, Vegosszeg) 
+            VALUES ('$az_id','$vezeteknev', '$keresztnev', '$idopont', '$idopont2', '$ora1', '$ora2', '$ora_input', '$ber_input', '$bonusz_input', '$vegosszeg_input')";
 
     if ($kapcsolat->query($sql)) 
     {
