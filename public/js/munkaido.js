f229465add0a353d6d6b2fd31db1a852ad685212
Papp Gyula <144594449+guts7777@users.noreply.github.com>
2025-04-04 10:57:40 +0200

    Feltétel frissítése v2

--- public/js/munkaido.js
+++ public/js/munkaido.js
@@ -63,10 +63,10 @@ function orak()
 
     }
 
-    if (ora_input.value < 0 || ora_input.value > 24)
+    if (ora_input.value < 1 || ora_input.value > 24)
     {
 
-        alert("Csak 0-től 24 óra!");
+        alert("Csak 1-től 24 óra!");
 
         ora_input.value = "";
 
@@ -89,10 +89,10 @@ function bonusz()
 
     }
 
-    if (bonusz_input.value < 1 || bonusz_input.value > 100)
+    if (bonusz_input.value < 0 || bonusz_input.value > 100)
     {
 
-        alert("Csak 1-től 100% bónusz!");
+        alert("Csak 0-től 100% bónusz!");
 
         bonusz_input.value = "";
 
